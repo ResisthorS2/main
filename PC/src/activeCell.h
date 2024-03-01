5546febe7cfe2609f8848551106d8444e5ab373a
Arthur-Olivier Fortin <aofor@ulaval.ca>
2024-02-29 19:13:57 -0500

    SafeSave pour Ced

--- PC/src/activeCell.h
+++ PC/src/activeCell.h
@@ -1,12 +1,7 @@
-
 #ifndef ACTIVECELL_H
 #define ACTIVECELL_H
 
-
 #include "cell.h"
-
-
-
 
 //direction
 #define LEFT 0
@@ -14,30 +9,75 @@
 #define UP 2
 #define DOWN 3
 
+/**
+ * @class ActiveCell
+ * @brief Classe représentant une cellule active, hérite de la classe Cell
+ */
 class ActiveCell : public Cell
 {
     public:
+        /**
+         * @brief Constructeur par défaut de la classe ActiveCell
+         */
         ActiveCell();
+
+        /**
+         * @brief Destructeur de la classe ActiveCell
+         */
         ~ActiveCell();
+
+        /**
+         * @brief Méthode pour obtenir l'orientation de la cellule active
+         * @return L'orientation de la cellule active : int{NONE=-1, north=0, south=1, east=2, west=3}
+         */
         int getOrientation();
+
+        /**
+         * @brief Méthode pour définir l'orientation de la cellule active
+         * @param orientation L'orientation à définir : int{NONE=-1, north=0, south=1, east=2, west=3}
+         */
         void setOrientation(int orientation);
+
+        /**
+         * @brief Méthode pour définir l'image de la cellule active
+         * @param cell La cellule active dont l'image doit être définie : ActiveCell*
+         */
         void setImageCell(ActiveCell *cell);
+
+        /**
+         * @brief Méthode pour déplacer la cellule active
+         * @param direction La direction du déplacement : int{LEFT=0, RIGHT=1, UP=2, DOWN=3}
+         */
         void move(int direction);
+
+        /**
+         * @brief Méthode pour copier une cellule dans la cellule active
+         * @param cell La cellule à copier : Cell*
+         */
         void cpyCell(Cell* cell);
+
+        /**
+         * @brief Méthode pour obtenir une cellule voisine de la cellule active
+         * @param orientation L'orientation de la cellule voisine à obtenir : int{NONE=-1, north=0, south=1, east=2, west=3}
+         * @return pointeur Cell* si la cellule voisine existe, NULL sinon
+         */
         Cell *getCellAround(int orientation);
+
+        /**
+         * @brief Méthode pour obtenir la clé de la cellule active
+         * @return La clé de la cellule active : int
+         */
         int getKey();
+
     private:
-        Cell* cell_north;
-        Cell* cell_south;
-        Cell* cell_east;
-        Cell* cell_west;
-        int orientation; /** @brief  north=0, south=1, east=2, west=3 */
-        int *type;
-        int *locked;
-        std::string ***imageCell;
-        int keys[1] = {1};
-        
-        
-        
+        Cell* cell_north; // Cellule voisine au nord
+        Cell* cell_south; // Cellule voisine au sud
+        Cell* cell_east; // Cellule voisine à l'est
+        Cell* cell_west; // Cellule voisine à l'ouest
+        int orientation; // Orientation de la cellule active int{NONE=-1, north=0, south=1, east=2, west=3}
+        int *type; // Type de la cellule active : int{VIDE=0, COULOIR=1, DOORNORTH=2, DOORSOUTH=3, DOOREAST=4, DOORWEST=5}
+        int *locked; // État de verrouillage de la cellule active : int{UNLOCKED=0, LOCKED=1}
+        std::string ***imageCell; // Image de la cellule active : int[3][3]
+        int keys[1] = {1}; // Clé de la cellule active
 };
 #endif
--- PC/src/cell.h
+++ PC/src/cell.h
@@ -1,17 +1,10 @@
 #ifndef CELL_H
 #define CELL_H
 
-
 #include <iostream>
-
-
-
-
-
 
 #define LOCKED 1
 #define UNLOCKED 0
-
 
 #define VIDE 0  
 #define COULOIR 1  
@@ -20,7 +13,6 @@
 #define DOOREAST 4  
 #define DOORWEST 5  
 
-
 const int NONE=-1;
 const int north=0;
 const int south=1;
@@ -28,45 +20,125 @@ const int east=2;
 const int west=3;
 const int maxRoom=50;
 
-
-
-
-
-
+/**
+ * @class Cell
+ * @brief Classe représentant une cellule, peut être une cellule vide, un couloir, une porte, etc.
+ */
 class Cell
 {
     public:
+        /**
+         * @brief Constructeur par défaut de la classe Cell
+         */
         Cell();
+
+        /**
+         * @brief Constructeur de la classe Cell avec un type spécifique
+         * @param type Le type de la cellule à créer : int{VIDE=0, COULOIR=1, DOORNORTH=2, DOORSOUTH=3, DOOREAST=4, DOORWEST=5}
+         */
         Cell(int type);
+
+        /**
+         * @brief Destructeur de la classe Cell
+         */
         virtual ~Cell();
-        /** @brief retourne le type de la Cell @return int{vide = 0, couloir = 1, classe = 2,}*/
+
+        /**
+         * @brief Méthode pour obtenir le type de la cellule
+         * @return Le type de la cellule : int{VIDE=0, COULOIR=1, DOORNORTH=2, DOORSOUTH=3, DOOREAST=4, DOORWEST=5}
+         */
         int *getType();
-        /** @brief retourne un int selon si la Cell est vérouillée @return 0 = UNLOCKED, 1 = LOCKED */
+
+        /**
+         * @brief Méthode pour définir le type de la cellule
+         * @param type Le type à définir : int{VIDE=0, COULOIR=1, DOORNORTH=2, DOORSOUTH=3, DOOREAST=4, DOORWEST=5}
+         */
         void setType(int type);
+
+        /**
+         * @brief Méthode pour définir l'image de la cellule
+         * @param cell La cellule dont l'image doit être définie : Cell*
+         */
         virtual void setImageCell(Cell *cell);
+
+        /**
+         * @brief Méthode pour imprimer la cellule dans le terminal
+         * @param line La ligne à imprimer : int{0 à 2}
+         * @return La ligne imprimée
+         */
         std::string printCellTerminal(int line);
+
+        /**
+         * @brief Méthode pour imprimer la cellule
+         */
         void printCell();
-        std::string imageCell[3][3];
+
+        /**
+         * @brief Méthode pour définir l'état de verrouillage de la cellule
+         * @param locked L'état de verrouillage à définir : int{UNLOCKED=0, LOCKED=1}
+         */
         void setLocked(int locked);
+
+        /**
+         * @brief Méthode pour obtenir l'état de verrouillage de la cellule
+         * @return L'état de verrouillage de la cellule : int{UNLOCKED=0, LOCKED=1}
+         */
         int *getLocked();
+
+        /**
+         * @brief Méthode pour définir une cellule voisine
+         * @param orientation L'orientation de la cellule voisine à définir : int{NONE=-1, north=0, south=1, east=2, west=3}
+         * @param cell La cellule voisine à définir : Cell*
+         */
         void setCellAround(int orientation, Cell* cell);
+
+        /**
+         * @brief Méthode pour obtenir une cellule voisine
+         * @param orientation L'orientation de la cellule voisine à obtenir : int{NONE=-1, north=0, south=1, east=2, west=3}
+         * @return Pointeur vers la cellule voisine dans l'orientation spécifiée ou NULL si aucune cellule n'est présente dans cette orientation
+         */
         Cell *getCellAround(int orientation);
+
+        /**
+         * @brief Méthode pour copier une cellule
+         * @param enterFrom La cellule à copier
+         */
         void cpyEnterFrom(int *enterFrom);
+
+        /**
+         * @brief Méthode pour entrer dans une cellule
+         * @param orientation L'orientation de l'entrée : int{NONE=-1, north=0, south=1, east=2, west=3}
+         * @param key La clé pour entrer dans la cellule : int[maxRoom]
+         * @return Pointeur vers la cellule entrée
+         */
         Cell *enterCell(int orientation, int key[maxRoom]);
+
+        /**
+         * @brief Méthode pour obtenir la cellule d'entrée
+         * @return Pointeur vers la cellule d'entrée
+         */
         int *getEnterFrom();
+
+        /**
+         * @brief Méthode pour définir la clé pour déverrouiller la cellule
+         * @param keyToUnlock La clé pour déverrouiller la cellule : int
+         */
         void setKeyToUnlock(int keyToUnlock);
+
+        /**
+         * @brief Méthode pour obtenir la clé pour déverrouiller la cellule
+         * @return La clé pour déverrouiller la cellule
+         */
         int getKeyToUnlock();
 
-        
-
     private:
-        int type;
-        Cell* cell_north;
-        Cell* cell_south;
-        Cell* cell_east;
-        Cell* cell_west;
-        int locked; /** @brief 0 = UNLOCKED, 1 = LOCKED */
-        int keyToUnlock;
+        int type; // Type de la cellule : int{VIDE=0, COULOIR=1, DOORNORTH=2, DOORSOUTH=3, DOOREAST=4, DOORWEST=5}
+        Cell* cell_north; // Cellule voisine au nord
+        Cell* cell_south; // Cellule voisine au sud
+        Cell* cell_east; // Cellule voisine à l'est
+        Cell* cell_west; // Cellule voisine à l'ouest
+        int locked; // État de verrouillage de la cellule : int{UNLOCKED=0, LOCKED=1}
+        int keyToUnlock; // Clé pour déverrouiller la cellule : int
         //Minigame minigame[];
 };
 #endif
